3ccd2d92a0a862d203b9b9f86cfce10f43958189
kogayushi <youshe.old.pleasure@gmail.com>
2019-11-28 21:37:50 +0900

    initial commit

---
+++ src/main/kotlin/com/example/enumrestcontrollerkotlin/EnumRestControllerKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.enumrestcontrollerkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnumRestControllerKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<EnumRestControllerKotlinApplication>(*args)
+}
